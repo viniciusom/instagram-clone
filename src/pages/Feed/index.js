d4dcf62a28faf6e581fa6529feb0de4758530fa6
Vinicius Moraes <viniciusom01@hotmail.com>
2023-03-28 00:55:40 -0300

    add feed pagination

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -5,19 +5,26 @@ import { Post, Header, Avatar, Name, PostImage, Description } from "./styles";
 
 const Feed = () => {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  const loadPage = async (pageNumber = page, shouldRefresh) => {
+    if(total && pageNumber > total) return;
+
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
+    );
+
+    const data = await response.json();
+    const totalItems = response.headers.get('X-Total-Count');
+
+    setTotal(Math.floor(totalItems / 5));
+    setFeed([...feed, ...data]);
+    setPage(pageNumber + 1);
+  };
 
   useEffect(() => {
-    const loadFeed = async () => {
-      const response = await fetch(
-        "http://localhost:3000/feed?_expand=author&_limit=5&_page=1"
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
-    };
-
-    loadFeed();
+    loadPage();
   }, []);
 
   return (
@@ -25,10 +32,13 @@ const Feed = () => {
       <FlatList
         data={feed}
         keyExtractor={(post) => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
         renderItem={({ item }) => (
           <Post>
             <Header>
               <Avatar source={{ uri: item.author.avatar }} />
+              <Name>{item.author.name}</Name>
             </Header>
 
             <PostImage ratio={item.aspectRatio} source={{ uri: item.image }} />
--- src/pages/Feed/styles.js
+++ src/pages/Feed/styles.js
@@ -1,19 +1,25 @@
 import styled from "styled-components";
 
 export const Post = styled.View`
-
+  margin-top: 10px;
 `;
 
 export const Header = styled.View`
-  
+  padding: 15px;
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const Avatar = styled.Image`
-  
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  margin-right: 10px;
 `;
 
 export const Name = styled.Text`
-  
+  color: #333;
+  font-weight: bold;
 `;
 
 export const PostImage = styled.Image`
@@ -22,5 +28,6 @@ export const PostImage = styled.Image`
 `;
 
 export const Description = styled.Text`
-  
+  padding: 15px;
+  line-height: 18px;
 `;
